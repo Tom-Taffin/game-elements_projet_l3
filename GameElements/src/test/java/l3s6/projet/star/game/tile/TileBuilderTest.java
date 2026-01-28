2d7ea9f2c8c478bb61e991fa8788abe3804192e0
Taffin Tom <taffin.tom59@gmail.com>
2026-01-28 22:30:44 +0100

    test: add testBuildTileWithRoadCorrectSyntaxe in TileBuilderTest

--- GameElements/src/test/java/l3s6/projet/star/game/tile/TileBuilderTest.java
+++ GameElements/src/test/java/l3s6/projet/star/game/tile/TileBuilderTest.java
@@ -3,6 +3,7 @@ package l3s6.projet.star.game.tile;
 import org.junit.jupiter.api.Test;
 
 import l3s6.projet.star.game.edge.EdgeNoRoad;
+import l3s6.projet.star.game.edge.EdgeWithRoad;
 import l3s6.projet.star.game.edge.Topology;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -31,6 +32,23 @@ public class TileBuilderTest {
         assertEquals(Topology.FIELD, ((EdgeNoRoad) tile.getEdge(Direction.RIGHT)).getZoneTopology());
         assertEquals(Topology.CITY, ((EdgeNoRoad) tile.getEdge(Direction.BOTTOM)).getZoneTopology());
         assertEquals(Topology.FIELD, ((EdgeNoRoad) tile.getEdge(Direction.LEFT)).getZoneTopology());
+    }
+
+    @Test
+    public void testBuildTileWithRoadCorrectSyntaxe() throws WrongTileSyntaxException{
+        String expString = "c-frf0-f0rf-c ";
+        Tile tile = this.tileBuilder.buildTile(expString);
+        assertFalse(tile.getEdge(Direction.TOP).hasRoad());
+        assertTrue(tile.getEdge(Direction.RIGHT).hasRoad());
+        assertTrue(tile.getEdge(Direction.BOTTOM).hasRoad());
+        assertFalse(tile.getEdge(Direction.LEFT).hasRoad());
+
+        assertEquals(Topology.CITY, ((EdgeNoRoad) tile.getEdge(Direction.TOP)).getZoneTopology());
+        assertEquals(Topology.FIELD, ((EdgeWithRoad) tile.getEdge(Direction.RIGHT)).getZone1Topology());
+        assertEquals(Topology.FIELD, ((EdgeWithRoad) tile.getEdge(Direction.RIGHT)).getZone2Topology());
+        assertEquals(Topology.FIELD, ((EdgeWithRoad) tile.getEdge(Direction.BOTTOM)).getZone1Topology());
+        assertEquals(Topology.FIELD, ((EdgeWithRoad) tile.getEdge(Direction.BOTTOM)).getZone2Topology());
+        assertEquals(Topology.CITY, ((EdgeNoRoad) tile.getEdge(Direction.LEFT)).getZoneTopology());
     }
 
 }
