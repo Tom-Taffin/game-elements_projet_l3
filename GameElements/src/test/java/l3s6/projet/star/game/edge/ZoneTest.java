573f3b3364793b2750be568c35f0c94cd0558390
Tom Taffin <tom.taffin2.etu@univ-lille.fr>
2026-02-04 10:14:17 +0100

    test: ZoneTest with test for connect zones

---
+++ GameElements/src/test/java/l3s6/projet/star/game/edge/ZoneTest.java
@@ -0,0 +1,18 @@
+package l3s6.projet.star.game.edge;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ZoneTest {
+    @Test
+    public void testConnectZone(){
+        Zone zone1 = new Zone(Topology.FIELD);
+        Zone zone2 = new Zone(Topology.FIELD);
+
+        zone1.connectTo(zone2);
+
+        assertTrue(zone1.getConnectingZones().contains(zone2));
+        assertTrue(zone2.getConnectingZones().contains(zone1));
+    }
+}
