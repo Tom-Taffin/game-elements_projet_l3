da9b9c9924b892e5ae8b08154405b1bd1eafd680
Tom Taffin <tom.taffin2.etu@univ-lille.fr>
2026-02-04 18:54:36 +0100

    feat: modif in TileBuilder

--- GameElements/src/main/java/l3s6/projet/star/game/tile/TileBuilder.java
+++ GameElements/src/main/java/l3s6/projet/star/game/tile/TileBuilder.java
@@ -1,6 +1,8 @@
 package l3s6.projet.star.game.tile;
 
 
+import java.util.HashMap;
+
 import l3s6.projet.star.game.edge.Edge;
 import l3s6.projet.star.game.edge.EdgeNoRoad;
 import l3s6.projet.star.game.edge.EdgeWithRoad;
@@ -10,6 +12,45 @@ import l3s6.projet.star.game.edge.Zone;
 public class TileBuilder {
     
     public TileBuilder() {
+    }
+
+    public Tile build(String string) throws WrongTileSyntaxException{
+        HashMap<String,Zone> zoneConnections = new HashMap<>();
+        Edge[] edges = new Edge[4];
+        this.analyseString(string, edges, zoneConnections);
+        Tile tile = new Tile(edges[0], edges[1], edges[2], edges[3]);
+        return tile;
+    }
+
+    private void analyseString(String string, Edge[] edges, HashMap<String,Zone> zoneConnections) throws WrongTileSyntaxException{
+        String[] stringEdges = string.split("-");
+        if(stringEdges.length != 4){
+            throw new WrongTileSyntaxException("There is no 3 '-'");
+        }
+        for(int i = 0 ; i < stringEdges.length ; i++){
+            if(stringEdges[i].contains("r")){
+                String[] stringZones = stringEdges[i].split("r");
+                EdgeWithRoad edge = this.buildEdgeWithRoad(stringZones);
+                edges[i] = edge;
+                zoneConnections.put(stringZones[0].substring(1),edge.getZone1());
+                zoneConnections.put(stringZones[1].substring(1),edge.getZone2());
+                
+            }
+            else{
+                EdgeNoRoad edge = this.buildEdgeNoRoad(stringEdges[i]);
+                edges[i] = edge;
+                zoneConnections.put(stringEdges[i].substring(1),edge.getZone());
+                
+            }
+        }
+    }
+
+    private EdgeNoRoad buildEdgeNoRoad(String string) throws WrongTileSyntaxException{
+        return new EdgeNoRoad(new Zone(this.getTopology(string)));
+    }
+
+    private EdgeWithRoad buildEdgeWithRoad(String[] stringZones) throws WrongTileSyntaxException{
+        return new EdgeWithRoad(new Zone(this.getTopology(stringZones[0])),new Zone(this.getTopology(stringZones[1])));
     }
 
     private Topology getTopology(String string) throws WrongTileSyntaxException{
@@ -22,31 +63,5 @@ public class TileBuilder {
         else{
             throw new WrongTileSyntaxException(string + " is not recognized");
         }
-    }
-
-    public Tile buildTile(String string) throws WrongTileSyntaxException{
-        Edge[] edges = new Edge[4];
-        String[] stringEdges = string.split("-");
-        if(stringEdges.length != 4){
-            throw new WrongTileSyntaxException("There is no 3 '-'");
-        }
-        for(int i = 0 ; i < 4 ; i++){
-            if(stringEdges[i].contains("r")){
-                edges[i] = buildEdgeWithRoad(stringEdges[i]);
-            }
-            else{
-                edges[i] = this.buildEdgeNoRoad(stringEdges[i]);
-            }
-        }
-        return new Tile(edges[0], edges[1], edges[2], edges[3]);
-    }
-
-    private EdgeNoRoad buildEdgeNoRoad(String string) throws WrongTileSyntaxException{
-        return new EdgeNoRoad(new Zone(this.getTopology(string)));
-    }
-
-    private EdgeWithRoad buildEdgeWithRoad(String string) throws WrongTileSyntaxException{
-        String[] stringZones = string.split("r");
-        return new EdgeWithRoad(new Zone(this.getTopology(stringZones[0])),new Zone(this.getTopology(stringZones[1])));
     }
 }
